7aa38ce11d428bf2448a34feafb9d81920989a20
johnnyCo15 <JonNguyen31@gmail.com>
2024-03-19 00:43:16 -0400

    updated NLPreader

--- NLPReader.py
+++ NLPReader.py
@@ -1,10 +1,6 @@
 import spacy
 import pandas as pd
 
-# loads spacy
-nlp = spacy.load('en_core_web_trf')
-locations = []
-
 # asks user for file path
 def file_finder():
     file_path = input("Enter the path of txt file: ")
@@ -14,21 +10,34 @@ def file_finder():
 def file_opener(file_path):
     with open(file_path, 'r') as file:
         lines = file.readlines()
-    text_dict = {i + 1: line.strip() for i, line in enumerate(lines)}
-    return text_dict
+    return {i + 1: line.strip() for i, line in enumerate(lines)}
+  
+# extract locations from text
+def extract_location(text_dict):
+    nlp = spacy.load('en_core_web_trf')
+    locations = []
+    for _, text in text_dict.items():
+        doc = nlp(text)
+        location_entities = [ent.text for ent in doc.ents if ent.label_ == "GPE" or ent.label_ == "LOC"]
+        location_str = ', '.join(location_entities)
+        locations.append(location_str)
+    return locations
 
 # create a dataframe from dict with key and value cols 
-def df_creator(text_dict):
+def create_df(text_dict, locations):
     df = pd.DataFrame(list(text_dict.items()), columns = ['key', 'value'])
+    df['locations'] = locations 
     return df
 
-# iterate over each key and search for locations
-def iter():
-    return 
-
-
+# main
 def main():
-    return 
+
+    file_path = file_finder()
+    text_dict = file_opener(file_path)
+    locations = extract_location(text_dict)
+    df = create_df(text_dict, locations)
+    print(df)
+
 
 if __name__ == "__main__":
     main()
