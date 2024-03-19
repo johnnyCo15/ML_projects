10e4e074f0985d01357e754d5b56c049d344d8be
johnnyCo15 <JonNguyen31@gmail.com>
2024-03-19 15:53:05 -0400

    updated dataInteroretation.py

--- dataInterpretation.py
+++ dataInterpretation.py
@@ -1,1 +1,11 @@
 import pandas as pd 
+
+# save file path
+melbourne_file_path = '/Users/jonnguyen/Downloads/GITHUB/ML_projects/melb_data.csv'
+
+# read/ store the data 
+melbourne_data = pd.read_csv(melbourne_file_path)
+
+# print summary of data
+print(melbourne_data.describe())
+
