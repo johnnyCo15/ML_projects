dd16d7f304da734436a98b2faaaefabe585c10bf
johnnyCo15 <JonNguyen31@gmail.com>
2024-03-19 00:36:24 -0400

    updated NLPreader

--- NLPReader.py
+++ NLPReader.py
@@ -1,4 +1,5 @@
 import spacy
+import pandas as pd
 
 # loads spacy
 nlp = spacy.load('en_core_web_trf')
@@ -16,7 +17,15 @@ def file_opener(file_path):
     text_dict = {i + 1: line.strip() for i, line in enumerate(lines)}
     return text_dict
 
-# 
+# create a dataframe from dict with key and value cols 
+def df_creator(text_dict):
+    df = pd.DataFrame(list(text_dict.items()), columns = ['key', 'value'])
+    return df
+
+# iterate over each key and search for locations
+def iter():
+    return 
+
 
 def main():
     return 
