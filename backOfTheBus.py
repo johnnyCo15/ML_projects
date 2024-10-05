6f3eabc091aa2b4cb085e16ff7c50419445104c4
johnnyCo15 <JonNguyen31@gmail.com>
2024-10-04 20:50:07 -0400

    updated backOfTheBus

--- backOfTheBus.py
+++ backOfTheBus.py
@@ -4,9 +4,7 @@ import pandas as pd
 import spacy as sp 
 
 
-file_path = '/Users/jonnguyen/Downloads/kaggle_bot_accounts.csv'
+file_path = '/Users/jonnguyen/Downloads/CSVs/kaggle_bot_accounts.csv'
 bot = pd.read_csv(file_path)
-print(bot.describe())
-
-
-
+#print(bot.describe())
+#print(bot.columns)
