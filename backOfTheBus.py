4785abbdecf207e34324f5e56bd1ed112b54e398
johnnyCo15 <JonNguyen31@gmail.com>
2024-12-12 17:01:42 -0500

    updated backOfTheBus.py and added yohji_links

--- backOfTheBus.py
+++ backOfTheBus.py
@@ -1,10 +1,40 @@
-# run source venv/bin/activate before running
+# Image scraper for DTP (Designer trend predictor)
+# run "source venv/bin/activate" before running
 
-import pandas as pd 
-import spacy as sp 
+import requests
+from bs4 import BeautifulSoup
+from striprtf.striprtf import rtf_to_text
 
+def getData(url):
+    r = requests.get(url)
+    return r.text
 
-file_path = '/Users/jonnguyen/Downloads/CSVs/kaggle_bot_accounts.csv'
-bot = pd.read_csv(file_path)
-#print(bot.describe())
-#print(bot.columns)
+def scrape_images(url):
+    htmldata = getData(url) 
+    soup = BeautifulSoup(htmldata, 'html.parser')
+    for item in soup.find_all('img'):
+        img_url = item.get('src', '')
+        if 'PICT' in img_url: 
+            print(img_url)
+
+def read_urls_from_txt(file_path):
+    with open(file_path, 'r') as file:
+        urls = [line.strip() for line in file.readlines() if line.strip()]  # Clean up the list
+    return urls
+
+def main():
+    print("starting...")
+    file_path = "/Users/jonnguyen/Downloads/GITHUB/ML_projects/Yohji_urls.txt"
+    
+    # Read URLs from the file
+    urls = read_urls_from_txt(file_path)
+    
+    # Loop through each URL and scrape images
+    for url in urls:
+        print(f"Scraping {url}...")
+        scrape_images(url)
+
+    print("finished.")
+
+if __name__ == "__main__":
+    main()
